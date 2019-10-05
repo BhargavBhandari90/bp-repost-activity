d1881779d113ed7d8aed75d94b2632be3ea73b41
Bhargav <bhargav@buddyboss.com>
2019-10-06 00:29:05 +0530

    Add group activity

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,7 +9,13 @@ var currentRequest = null;
     init: function init() {
       this.bprpa_repost();
       this.bprpa_set_param();
+      this.bprpa_reset_form();
+      this.bprpa_show_whereto_post();
     },
+
+    /**
+     * Set perameter in ajax request for post update.
+     */
     bprpa_set_param: function bprpa_set_param() {
       $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
         // Check if form is available or not.
@@ -26,28 +32,72 @@ var currentRequest = null;
           return true;
         }
 
-        var original_activity_id = $('.bp-repost-activity.repost-selected').data('activity_id'); // Set form data into activity ajax.
+        var original_activity_id = $('#repost-activity-form #original_item_id').val(),
+            posting_at = $('#repost-activity-form #posting_at').val(),
+            group_id = $('#repost-activity-form #rpa_group_id').val(),
+            group_args = '';
 
-        if (typeof original_activity_id !== 'undefined' && originalOptions.data.action === 'post_update') {
-          options.data += '&original_item_id=' + original_activity_id + '&content=repost';
+        if ('undefined' !== typeof posting_at && 'groups' === posting_at) {
+          group_args = '&object=groups&item_id=' + group_id;
+        } // Set form data into activity ajax.
+
+
+        if (typeof original_activity_id !== 'undefined' && '' !== original_activity_id && originalOptions.data.action === 'post_update') {
+          options.data += '&original_item_id=' + original_activity_id + '&content=repost' + group_args;
+        }
+      });
+      $(document).ajaxComplete(function (event, xhr, settings) {
+        // Get ajax data.
+        var setting_data = settings.data; // If it's related to spotlight, then run the script.
+
+        if (typeof setting_data !== 'undefined' && setting_data.indexOf('original_item_id') != -1) {
+          $('#repost-box').modal('hide');
         }
       });
     },
     bprpa_repost: function bprpa_repost() {
+      // When we submit repost form.
+      $(document).on('submit', '#repost-activity-form', function (e) {
+        e.preventDefault(); // Submit buddypress update form.
+
+        $('#aw-whats-new-submit').trigger('click');
+      }); // Set data in hidden fields when we click on repost button.
+
       $(document).on('click', '.bp-repost-activity', function (e) {
         e.preventDefault();
-
-        if ($('.bp-repost-activity').hasClass('repost-selected')) {
-          $('.bp-repost-activity').removeClass('repost-selected');
-        }
-
-        $(this).addClass('repost-selected');
         var activity_id = $(this).data('activity_id'),
-            original_content = $('#activity-stream #activity-' + activity_id + ' .activity-inner').html();
-        console.log(original_content);
-        console.log(activity_id);
-        $('#repost-activity-form #original_item_id').val(activity_id);
-        $('#repost-activity-form .content').html(original_content); // $('#aw-whats-new-submit').trigger( 'click' );
+            original_content = $('#activity-stream #activity-' + activity_id + ' .activity-inner').html(); // Set values in hidden fields.
+
+        $('#repost-activity-form #original_item_id').val(activity_id); // Show content in popup which we are going to repost.
+
+        $('#repost-activity-form .content').html(original_content);
+      });
+    },
+
+    /**
+     * Reset form when popup is closed.
+     */
+    bprpa_reset_form: function bprpa_reset_form() {
+      $('#repost-box').on('hidden.bs.modal', function () {
+        $('#repost-activity-form #original_item_id').val('');
+        $('#repost-activity-form #posting_at').val('');
+      });
+    },
+
+    /**
+     * Show groups when select group from dropdown.
+     */
+    bprpa_show_whereto_post: function bprpa_show_whereto_post() {
+      $(document).on('change', '#posting_at', function () {
+        var posting_at = $(this).val(),
+            group_selector = $('#rpa_group_id'); // Display group dropdown if selected group.
+
+        if ('undefined' !== typeof posting_at && 'groups' === posting_at) {
+          group_selector.show();
+        } else {
+          // Hide otherwise.
+          group_selector.hide();
+        }
       });
     }
   };
